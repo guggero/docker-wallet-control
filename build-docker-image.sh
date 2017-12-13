0e776f523e93921be455ec7d613426fcf3fb8ba9
Oliver Gugger <gugger@puzzle.ch>
2017-12-13 16:37:45 +0100

    Fix invalid docker build path, remove old build script

--- build-docker-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LATEST_TAG=$(git describe --abbrev=0 --tags)
-DOCKER_NAME=guggero/docker-wallet-control
-
-docker build -t $DOCKER_NAME -t $DOCKER_NAME:latest -t $DOCKER_NAME:$LATEST_TAG .
-docker push $DOCKER_NAME
-docker push $DOCKER_NAME:latest
-docker push $DOCKER_NAME:$LATEST_TAG
